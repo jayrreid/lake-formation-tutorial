f50732c54837068ac10917e3a87eb4d7938e5e9a
JR <jay.r.reid@gmail.com>
2021-02-15 11:50:48 -0600

    added assume role policy

--- main.tf
+++ main.tf
@@ -4,8 +4,25 @@ provider "aws" {
   region  = var.region
 }
 
-data "aws_iam_role" "DataLakeWorkflowRole" {
+resource "aws_iam_role" "DataLakeWorkflowRole" {
   name ="DataLakeWorkflowRole"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
 }
 
 resource "aws_iam_role_policy" "DataLakeWorkflowRolePolicy" {
