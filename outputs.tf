2ffa18d804777fe7eab1de12a900d094fa101190
JR <jay.r.reid@gmail.com>
2021-02-15 16:57:17 -0600

    added data lake administrator user to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "DataLakeWorkflowRole" {
   description = "datalake workflow role"
   value       = aws_iam_role.DataLakeWorkflowRole
 }
+
+output "DataLakeAdministrator" {
+  description = "datalake administrator"
+  value       = aws_iam_user.dl_admin
+}
